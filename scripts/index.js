11affaa80c8b9ae5497e952b3162ace2db528c49
Zeeshan Nasir <zeeeshannasir94@gmail.com>
2022-02-23 22:58:43 +0530

    worked on front page

--- scripts/index.js
+++ scripts/index.js
@@ -2,10 +2,11 @@ import { navbar } from "../components/navbar.js";
 
 document.getElementById("navbar").innerHTML = navbar();
 
-document.getElementById("icon").addEventListener("click",()=>{
+document.getElementById("icon").addEventListener("click", () => {
     window.location.href = "index.html";
 });
 
-document.getElementById("userPic").addEventListener("click",()=>{
+document.getElementById("userPic").addEventListener("click", () => {
     window.location.href = "profile.html";
 });
+
