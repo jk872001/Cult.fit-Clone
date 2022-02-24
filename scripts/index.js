e909f58f5b4cb80c44d80eb81921b934040c8bb5
Zeeshan Nasir <zeeeshannasir94@gmail.com>
2022-02-24 15:33:03 +0530

    finished front page and added footer component

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,8 @@
 import { navbar } from "../components/navbar.js";
+import { footer } from "../components/footer.js";
 
 document.getElementById("navbar").innerHTML = navbar();
+document.getElementById("footer").innerHTML = footer();
 
 document.getElementById("icon").addEventListener("click", () => {
     window.location.href = "index.html";
